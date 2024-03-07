4279ef9d9a07f784dcbc85f353ca5a0da0a23b30
zh l <1811226078@qq.com>
2024-03-07 17:37:23 +0800

    Merge remote-tracking branch 'sql/main'
    # Conflicts:
    #	生产切割/test1.sql

--- 生产切割/test.sql
+++ 生产切割/test.sql
@@ -3,8 +3,8 @@ with drrk as (
     SELECT year(b.fdate)   AS '年份',
            month(b.fdate)  AS '月',
            day(b.fdate)    as '日',
-           SUM(a.FREALQTY) AS '汇总实收数量',
-           E.FNAME
+           SUM(a.FREALQTY) AS '汇总实收数量'
+
     FROM T_PRD_INSTOCKENTRY a
              LEFT JOIN
          T_PRD_INSTOCK b ON b.fid = a.fid
@@ -15,12 +15,12 @@ with drrk as (
       AND B.FDATE < CONVERT(date, GETDATE())
 
 
-    GROUP BY E.FNAME, b.fdate),
+    GROUP BY b.fdate),
 
      dyrk as (
 --入库单每月
-         SELECT SUM(a.FREALQTY) AS '汇总实收数量',
-                E.FNAME
+         SELECT SUM(a.FREALQTY) AS '汇总实收数量'
+
          FROM T_PRD_INSTOCKENTRY a
                   LEFT JOIN
               T_PRD_INSTOCK b ON b.fid = a.fid
@@ -42,26 +42,22 @@ with drrk as (
                  OR
              (DATEPART(YEAR, DATEADD(DAY, -1, GETDATE())) = DATEPART(YEAR, B.FDATE) AND
               DATEPART(MONTH, DATEADD(DAY, -1, GETDATE())) = DATEPART(MONTH, B.FDATE))
-             )
-
-         GROUP BY E.FNAME),
+             )),
      --车间每日每月领料
      lrll as (SELECT YEAR(b.fdate)     AS '年',
                      MONTH(b.fdate)    AS '月',
                      DAY(b.fdate)      AS '日',
-                     SUM(a.FACTUALQTY) AS '汇总实发数量',
-                     e.FNAME
+                     SUM(a.FACTUALQTY) AS '汇总实发数量'
+
               FROM T_PRD_PICKMTRLDATA a
                        LEFT JOIN T_PRD_PICKMTRL b ON b.fid = a.fid
                        LEFT JOIN T_BD_DEPARTMENT_L E ON E.FDEPTID = b.FWORKSHOPID
               WHERE b.FPRDORGID = '100329'
                 AND B.FDATE >= DATEADD(day, -1, CONVERT(date, GETDATE()))
                 AND B.FDATE < CONVERT(date, GETDATE())
+              GROUP BY b.fdate),
+     myll as (SELECT SUM(a.FACTUALQTY) AS '汇总实发数量'
 
-
-              GROUP BY e.FNAME, b.fdate),
-     myll as (SELECT SUM(a.FACTUALQTY) AS '汇总实发数量',
-                     e.FNAME
               FROM T_PRD_PICKMTRLDATA a
                        LEFT JOIN T_PRD_PICKMTRL b ON b.fid = a.fid
                        LEFT JOIN T_BD_DEPARTMENT_L e ON e.FDEPTID = b.FWORKSHOPID
@@ -79,31 +75,21 @@ with drrk as (
                       OR
                   (DATEPART(YEAR, DATEADD(DAY, -1, GETDATE())) = DATEPART(YEAR, B.FDATE) AND
                    DATEPART(MONTH, DATEADD(DAY, -1, GETDATE())) = DATEPART(MONTH, B.FDATE))
-                  )
-              GROUP BY e.FNAME),
-     ZH AS (select
---     YEAR(GETDATE())  AS 年,
---     MONTH(GETDATE()) AS 月,
---     DAY(GETDATE())   AS 月,
-temp.领料单当天数据,
-temp.领料单当月数据,
-a.汇总实收数量 AS 入库单当天数据,
-b.汇总实收数量 AS 入库单当月数据,
-b.FNAME
-            from drrk a
-                     right JOIN dyrk B ON A.FNAME = B.FNAME
-                     left join (select YEAR(GETDATE())  AS 年,
-                                       MONTH(GETDATE()) AS 月,
-                                       DAY(GETDATE())   AS 日,
-                                       a.汇总实发数量   AS 领料单当天数据,
-                                       b.汇总实发数量   AS 领料单当月数据,
-                                       b.FNAME
-                                from lrll a
-                                         right JOIN MYLL B ON A.FNAME = B.FNAME) temp on temp.FNAME = b.FNAME)
+                  ))
 
-SELECT SUM(ZH.领料单当天数据) AS 领料单当天数据,
-       SUM(ZH.领料单当月数据) AS 领料单当月数据,
-       SUM(ZH.入库单当天数据) AS 入库单当天数据,
-       SUM(ZH.入库单当月数据) AS 入库单当月数据
-FROM ZH
-
+select temp.领料单当天数据,
+       temp.领料单当月数据,
+       SUM(a.汇总实收数量) AS 入库单当天数据总数,
+       SUM(b.汇总实收数量) AS 入库单当月数据总数
+from drrk a
+         right JOIN dyrk B ON A.FNAME = B.FNAME
+         left join (select YEAR(GETDATE())  AS 年,
+                           MONTH(GETDATE()) AS 月,
+                           DAY(GETDATE())   AS 日,
+                           a.汇总实发数量   AS 领料单当天数据,
+                           b.汇总实发数量   AS 领料单当月数据,
+                           b.FNAME
+                    from lrll a
+                             right JOIN MYLL B ON A.FNAME = B.FNAME) temp on temp.FNAME = b.FNAME
+GROUP BY temp.领料单当天数据, temp.领料单当月数据
+ORDER BY temp.领料单当天数据;
