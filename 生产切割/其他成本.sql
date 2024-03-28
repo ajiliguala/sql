4d3c9bb0ae683b38d4e94f88fbb69469ef3f7ef1
zh l <1811226078@qq.com>
2024-03-28 11:13:49 +0800

    Merge remote-tracking branch 'sql/main'
    # Conflicts:
    #	生产切割/test1.sql

--- 生产切割/其他成本.sql
+++ 生产切割/其他成本.sql
@@ -1,4 +1,9 @@
-SELECT QTCB.FDATE, QTCB.FNUMBER, QTCB.FNAME, ISNULL(YFD.单价, 0) AS 单价, QTCB.FQTY,ISNULL(YFD.单价 * QTCB.FQTY, 0) AS 金额
+SELECT QTCB.FDATE,
+       QTCB.FNUMBER,
+       QTCB.FNAME,
+       ISNULL(YFD.单价, 0)             AS 单价,
+       QTCB.FQTY,
+       ISNULL(YFD.单价 * QTCB.FQTY, 0) AS 金额
 FROM (SELECT A.FDATE, C.FNUMBER, B.FQTY, E.FNAME
       FROM T_STK_MISDELIVERY A
                INNER JOIN T_STK_MISDELIVERYENTRY B ON A.FID = B.FID
@@ -15,13 +20,15 @@ FROM (SELECT A.FDATE, C.FNUMBER, B.FQTY, E.FNAME
                     FROM (SELECT ROW_NUMBER() OVER ( PARTITION BY WL.FNUMBER ORDER BY A.FCreateDate DESC ) AS XUHAO,
                                  B.FMATERIALID,
                                  WL.FNUMBER,
-                                 B.FNOTAXAMOUNT / B.FPRICEQTY                                              AS 单价
+                                 CASE
+                                     WHEN B.FPRICEQTY <> 0 THEN B.FNOTAXAMOUNT / B.FPRICEQTY
+                                     ELSE NULL
+                                     END                                                                   AS 单价
                           FROM T_AP_PAYABLE A
                                    INNER JOIN T_AP_PAYABLEENTRY B ON A.FID = B.FID
-                                   INNER JOIN (SELECT A.FMATERIALID,
-                                                      A.FNUMBER
-                                               FROM T_BD_MATERIAL A) WL ON B.FMATERIALID = WL.FMATERIALID
+                                   INNER JOIN (SELECT A.FMATERIALID, A.FNUMBER FROM T_BD_MATERIAL A) WL
+                                              ON B.FMATERIALID = WL.FMATERIALID
                           WHERE 1 = 1
-                            AND A.FDATE >= DATEADD(YEAR, DATEDIFF(YEAR, 0, GETDATE()), 0)
+                            AND A.FDATE >= DATEADD(YEAR, DATEDIFF(YEAR, 0, GETDATE()) - 1, 0)
                             AND A.FDOCUMENTSTATUS = 'C') T
                     WHERE T.XUHAO = 1) YFD ON QTCB.FNUMBER = YFD.FNUMBER
